a0894cc14ee1ffe0b0270dbd2750bd6844e78dbd
DmitryDemid <dimon.demid15@gmail.com>
2020-09-18 12:47:23 -0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <div className={s.wrapper}>
             <header className={s.header}>
-
+                <h1>Hello</h1>
             </header>
         </div>
     );
